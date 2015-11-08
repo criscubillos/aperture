f812834b9b2e66034fbb567e528163457f91eb5c
cristian <cristian@localhost>
2015-11-08 13:09:43 -0300

    primer commit

---
+++ test1/src/test/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author cristian
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        String hola = "";
+    }
+}
